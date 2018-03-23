e0254e79769ee50bea91e56b9c723c0d40225d9e
allanstarr023 <37152409+allanstarr023@users.noreply.github.com>
2018-03-23 11:27:41 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 document.addEventListener('deviceready', .onDeviceReady, false);
 
-function onDeviceReady(){
-	console.log('Device Ready...');
+function onDeviceReady() {
+    document.addEventListener("backbutton", function (e) {
+        e.preventDefault(); 
+    navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
+    }, false );
+}
+ 
+function onConfirmExit(button) {
+    if(button==2){ //If User select a No, then return back;
+        return;
+    }else{
+        navigator.app.exitApp(); // If user select a Yes, quit from the app.
+    }
 }
